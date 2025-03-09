f8cb7b3b1d00898d97746402cef07d89c87806ad
blagodieyoung <blagodieyoung@gmail.com>
2025-03-09 14:38:28 +0300

    fix double system message

--- src/main/java/net/cchat/cchatmod/mixins/ChatListenerMixin.java
+++ src/main/java/net/cchat/cchatmod/mixins/ChatListenerMixin.java
@@ -1,7 +1,6 @@
 package net.cchat.cchatmod.mixins;
 
 import net.cchat.cchatmod.gui.chat.CChatModEvents;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.chat.ChatListener;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -17,9 +16,9 @@ public class ChatListenerMixin {
     @Inject(method = "handleSystemMessage", at = @At("HEAD"), cancellable = true)
     private void onHandleSystemMessage(Component message, boolean overlay, CallbackInfo ci) {
         if (!overlay) {
-            Minecraft.getInstance().execute(() -> {
-                CChatModEvents.getInstance().addMessage(message.getString(), new ResourceLocation(MOD_ID, "textures/gui/system_icon.png"));
-            });
+            CChatModEvents.getInstance().addMessage(message.getString(),
+                    new ResourceLocation(MOD_ID, "textures/gui/system_icon.png"));
+            ci.cancel();
         }
     }
 }
